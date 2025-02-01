6c7bb20adcdb529ea1def97545ba0c002171ebe9
Matilda Gaddi <gaddi.mati@ucsd.edu>
2025-01-31 16:19:28 -0800

    add js for projects and homepage

--- global.js
+++ global.js
@@ -90,3 +90,47 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.colorScheme = value;
   });
 });
+
+
+
+export async function fetchJSON(url) { //export causes errors and nav menu disappears
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      console.log(response)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        console.log(response)
+      }
+      const data = await response.json();
+      return data; 
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+
+}
+// fetchJSON('../lib/projects.json');
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+export function renderProjects(projects, containerElement, headingLevel = 'h2') { //project is list
+  
+  containerElement.innerHTML = '';
+  for (let project of projects) { 
+  const article = document.createElement('article');
+  let img_here = project.image
+  if (!ARE_WE_HOME && !img_here.startsWith('http')) {
+    img_here = '../' + img_here;
+  }
+  article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${img_here}" alt="${project.title}" style="width: 300px; height: auto;">
+    <p>${project.description}</p>
+`; 
+  containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
